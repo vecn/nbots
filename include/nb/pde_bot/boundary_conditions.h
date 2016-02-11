2688f30cdb767daa40a685db9f25bb1688690a4e
Victor E. Cardoso Nungaray <victorc@cimat.mx>
2016-02-11 00:06:26 -0600

    Bcond struct redefined with iterator

--- include/nb/pde_bot/boundary_conditions.h
+++ include/nb/pde_bot/boundary_conditions.h
@@ -1,34 +1,32 @@
 #ifndef __NB_PDE_BOT_BOUNDARY_CONDITIONS_H__
 #define __NB_PDE_BOT_BOUNDARY_CONDITIONS_H__
 
-#include "nb/geometric_bot/mesh/elements2D/triangles.h"
+#include <stdint.h>
 
-typedef struct {
-	uint32_t N;
-	uint32_t *id;
-	bool *dof_mask;
-	double *val;
-} bc_data_t;
+#include "vcn/container_bot.h"
 
-typedef struct {
-	/* TEMPORAL: Change Neuman for Neumann */
-	/* Boundary conditions */
-	uint8_t N_dof; /* Degrees of freedom */
-	bc_data_t dirichlet_on_vtx;
-	bc_data_t neumann_on_vtx;
-	bc_data_t dirichlet_on_sgm;
-	bc_data_t neumann_on_sgm;
-} vcn_bcond_t;
+typedef enum {
+	NB_DIRICHLET,
+	NB_NEUMANN
+} nb_bcond_id;
 
-vcn_bcond_t* vcn_fem_bcond_create(void);
-vcn_bcond_t* vcn_fem_bcond_clone(const vcn_bcond_t *const bconditions);
-vcn_bcond_t* vcn_fem_bcond_read(const char* filename);
-void vcn_fem_bcond_save(const vcn_bcond_t *const bconditions, 
-			const char* filename);
-void vcn_fem_bcond_copy(vcn_bcond_t* cpy,
-			const vcn_bcond_t *const src);
-void vcn_fem_bcond_clear(vcn_bcond_t* bconditions);
-void vcn_fem_bcond_destroy(vcn_bcond_t* bconditions);
-void vcn_fem_bcond_printf(const vcn_bcond_t* const bconditions);
+typedef enum {
+	NB_BC_ON_POINT,
+	NB_BC_ON_SEGMENT
+} nb_bcond_where;
+
+typedef struct nb_bcond_s nb_bcond_t;
+
+nb_bcond_t* nb_bcond_create(uint8_t N_dof);
+nb_bcond_t* nb_bcond_clone(const nb_bcond_t *const bcond);
+nb_bcond_t* nb_bcond_read(vcn_cfreader_t *cfreader);
+void nb_bcond_save(const nb_bcond_t *const bcond,
+		   const char* filename);
+void nb_bcond_copy(nb_bcond_t* dest, const nb_bcond_t *const src);
+void nb_bcond_clear(nb_bcond_t* bcond);
+void nb_bcond_destroy(nb_bcond_t* bcond);
+void nb_bcond_push(nb_bcond_t *bcond, nb_bcond_id type_id,
+		   nb_bcond_where type_elem, uint32_t elem_id,
+		   bool dof_mask[], double value[]);
 
 #endif
