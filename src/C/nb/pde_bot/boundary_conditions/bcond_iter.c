65ee2ce92a216a4d86d79ca6309cf3d7a1950d42
Victor E. Cardoso Nungaray <victorc@cimat.mx>
2016-02-12 22:41:03 -0600

    Boundary conditions implemented

--- src/C/nb/pde_bot/boundary_conditions/bcond_iter.c
+++ src/C/nb/pde_bot/boundary_conditions/bcond_iter.c
@@ -8,25 +8,102 @@
 #include "nb/pde_bot/boundary_conditions/bcond_iter.h"
 
 #include "bc_atom.h"
+#include "bc_get_container.h"
 
+struct nb_bcond_iter_s {
+	bc_atom_t *bc;
+	vcn_iterator_t *internal;
+};
 
-typedef struct nb_bcond_iter_s nb_bcond_iter_t;
+static void* malloc_bcond_iter(void);
 
-uint16_t nb_bcond_iter_get_memsize(void);
-void nb_bcond_iter_init(void *iter_ptr);
-void nb_bcond_iter_copy(void *iter_ptr, const void *src_iter_ptr);
-void nb_bcond_iter_clear(void *iter_ptr);
+inline uint16_t nb_bcond_iter_get_memsize(void)
+{
+	return vcn_iterator_get_memsize() + sizeof(nb_bcond_iter_t);
+}
 
-void* nb_bcond_iter_create(void);
-void* nb_bcond_iter_clone(const void *iter_ptr);
-void nb_bcond_iter_destroy(void *iter_ptr);
+inline void nb_bcond_iter_init(void *iter_ptr)
+{
+	char *memblock = iter_ptr;
+	nb_bcond_iter_t *iter = iter_ptr;
+	iter->bc = NULL;
+	iter->internal = memblock + sizeof(nb_bcond_iter_t);
+	vcn_iterator_init(iter->internal);
+}
 
-void nb_bcond_iter_set_conditions(nb_bcond_iter_t *iter, nb_bcond_t *bcond,
-				  nb_bcond_id type_id,
-				  nb_bcond_where type_elem);
-bool nb_bcond_iter_has_more(const nb_bcond_iter_t *iter);
-void nb_bcond_iter_go_next(nb_bcond_iter_t *iter);
+inline void nb_bcond_iter_copy(void *iter_ptr, const void *src_iter_ptr)
+{
+	nb_bcond_iter_t *iter = iter_ptr;
+	nb_bcond_iter_t *src_iter = src_iter_ptr;
+	iter->bc = src_iter->bc;
+	vcn_iterator_copy(iter->internal, src_iter->internal);
+}
 
-uint32_t nb_bcond_iter_get_id(const nb_bcond_iter_t *iter);
-bool nb_bcond_iter_get_mask(const nb_bcond_iter_t *iter, uint8_t dof_id);
-double nb_bcond_iter_get_val(const nb_bcond_iter_t *iter, uint8_t dof_id);
+inline void nb_bcond_iter_clear(void *iter_ptr)
+{
+	nb_bcond_iter_t *iter = iter_ptr;
+	iter->bc = NULL;
+	vcn_iterator_clear(iter->internal);
+}
+
+inline void* nb_bcond_iter_create(void)
+{
+	void *iter = malloc_bcond_iter();
+	nb_bcond_iter_init(iter);
+}
+
+static void* malloc_bcond_iter(void)
+{
+	uint16_t size = nb_bcond_iter_get_memsize();
+	return malloc(size);
+}
+
+inline void* nb_bcond_iter_clone(const void *iter_ptr)
+{
+	void *iter = malloc_bcond_iter();
+	nb_bcond_iter_copy(iter, iter_ptr);
+	return iter;
+}
+
+inline void nb_bcond_iter_destroy(void *iter_ptr)
+{
+	nb_bcond_iter_clear(iter_ptr);
+	free(iter_ptr);
+}
+
+inline void nb_bcond_iter_set_conditions(nb_bcond_iter_t *iter,
+					 nb_bcond_t *bcond,
+					 nb_bcond_id type_id,
+					 nb_bcond_where type_elem)
+{
+	vcn_container_t *container = 
+		nb_bcond_get_container(bcond, type_id, type_elem);
+	vcn_iterator_set_container(iter->internal, container);
+}
+
+inline bool nb_bcond_iter_has_more(const nb_bcond_iter_t *iter)
+{
+	vcn_iterator_has_more(iter->internal);
+}
+
+inline void nb_bcond_iter_go_next(nb_bcond_iter_t *iter)
+{
+	iter->bc = vcn_iterator_get_next(iter->internal);
+}
+
+inline uint32_t nb_bcond_iter_get_id(const nb_bcond_iter_t *iter)
+{
+	return iter->bc->id;
+}
+
+inline bool nb_bcond_iter_get_mask(const nb_bcond_iter_t *iter,
+				   uint8_t dof_id)
+{
+	return iter->bc->mask;
+}
+
+inline double nb_bcond_iter_get_val(const nb_bcond_iter_t *iter,
+				    uint8_t dof_id)
+{
+	return iter->bc->value;
+}
