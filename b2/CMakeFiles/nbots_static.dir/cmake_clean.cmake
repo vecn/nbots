8189afa7744d21dbd5b009f0364c730fb0606522
Victor Cardoso <victorc@cimat.mx>
2017-01-18 12:46:05 -0600

    Implementing damage reader

--- b2/CMakeFiles/nbots_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libnbots.pdb"
-  "libnbots.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/nbots_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
